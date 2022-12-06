5ed246cf637528516423c6d75d2736af387a767f
Michael <114730461+Mmichael33-github@users.noreply.github.com>
2022-12-06 20:35:49 +0200

    my change again again 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 4500)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 12)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 18)
 
     Point = namedtuple('Point', 'x y')
     data = []
